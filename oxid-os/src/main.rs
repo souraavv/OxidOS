8a7f8a2135a9268dbb5e0390765ab0ca3fcfa663
Sourav Sharma <souravsharma2468@gmail.com>
2026-02-08 13:11:32 +0530

    Using Exit device, port-mapped IO

--- oxid-os/src/main.rs
+++ oxid-os/src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] //sets the name of the entry point
 
 // core is effectively: the minimum implementation of the Rust language itself.
 use core::panic::PanicInfo;
@@ -21,5 +24,44 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker 
     // looks for a function named `_start` by default
     println!("Hello world{}", "Sourav");
+
+    #[cfg(test)]
+    test_main();
+    exit_qemu(QemuExitCode::Success);
+
     loop {}
 }
+
+// Exiting Qemu for test case runs
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    println!("trivial assertion");
+    assert_eq!(1, 1);
+    println!("[ok]")
+}
+
